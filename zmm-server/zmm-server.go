ad0279506be8529fcb61b6a143ee67e98e02a643
Carl J. Mosca <carljmosca@gmail.com>
2017-04-24 22:18:10 -0400

    refactored

--- zmm-server/zmm-server.go
+++ zmm-server/zmm-server.go
@@ -68,10 +68,10 @@ func main() {
 
 
 	// protected url group
-	private := router.Group("/auth")
+	private := router.Group("/api")
 	private.Use(google.Auth())
 	private.GET("/", UserInfoHandler)
-	private.GET("/api", func(ctx *gin.Context) {
+	private.GET("/hello", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"message": "Hello from private for groups"})
 	})
 
